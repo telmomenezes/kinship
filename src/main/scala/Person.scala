5674625929b91046116251ca3e9100fe11f0c3a3
Telmo Menezes <telmo@telmomenezes.com>
2012-01-30 12:33:26 +0100

    Person class converted to Scala

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -1,21 +1,24 @@
 package kinship
 
 
+import scala.collection.mutable.Set
+
+
 class Person(id: Int, name: String, sex: String, fatherId: Int, motherId: Int) {
 	
-	val descendents = new Set[Int]
+	val descendents = Set[Int]()
 	var totalDesc = 0
 	
-	def addDescendent(descId: Int) {
-		descendents += descId)
+	def addDescendent(descId: Int) = {
+		descendents += descId
 		totalDesc += 1
 	}
 	
-	def isDescendent(descId: Int): Boolean {
+	def isDescendent(descId: Int): Boolean = {
 		descendents.contains(descId)
 	}
 	
-	override def toString: String {
+	override def toString: String  = {
 		"name: " + name + "; sex: " + sex + "; father: " + fatherId + "; mother: " + motherId
 	}
 }

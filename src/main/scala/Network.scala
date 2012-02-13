cca5b9c405c3d5dce008977b173e7d83979e9634
Telmo Menezes <telmo@telmomenezes.com>
2012-02-13 18:06:33 +0100

    siblings

--- src/main/scala/Network.scala
+++ src/main/scala/Network.scala
@@ -3,6 +3,7 @@ package com.telmomenezes.kinship
 import com.telmomenezes.Aux._
 import scala.io.Source
 import scala.util.Random
+import scala.collection.mutable.{Map => MMap}
 
 class Network (peopleList: List[Array[String]]) {
   val people: Map[Int, Person] = (for (item <- peopleList) yield (item(0).toInt, new Person(item))).toMap
@@ -15,6 +16,13 @@ class Network (peopleList: List[Array[String]]) {
     ++ (for (p <- people.values if p.motherId > 0) yield (p.motherId, p.id))).toArray
 
   val rand = new Random()
+
+  def updateChildren() = {
+    for (p <- people.values) {
+      if (p.fatherId > 0) people(p.fatherId).addChild(p.id)
+      if (p.motherId > 0) people(p.motherId).addChild(p.id)
+    }
+  }
 
   def updateDescendents(origId: Int, targ: Person, visited: List[Int]): Unit = {
     if (!(visited contains targ.id)) {
@@ -43,7 +51,41 @@ class Network (peopleList: List[Array[String]]) {
     for (p <- people.values)
       p.clearDescendents()
   }
+
+  def updateSiblings() = {
+    for (p <- people.values) {
+      val children = p.children.toArray
+      val n = children.size
+
+      for (i <- 0 until n) {
+        for (j <- (i + 1) until n) {
+          val pi = people(children(i))
+          val pj = people(children(j))
+          // only consider full siblings
+          if ((pi.fatherId == pj.fatherId) && (pi.motherId == pj.motherId)) {
+            pi.addSibling(pj.id)
+            pj.addSibling(pi.id)
+          }
+        }
+      }
+    }
+  }
+
+  def clearSiblings() = {
+    for (p <- people.values)
+      p.clearSiblings()
+  }
   
+  def sibCounts: MMap[Int, Int] = {
+    val map = MMap[Int, Int]()
+    for (p <- people.values) {
+      val sibCount = p.siblings.size
+      if (map contains sibCount) map(sibCount) += 1
+      else map(sibCount) = 1 
+    }
+    map
+  }
+
   def avgDescendents: Double = {
     var avg = 0.0
     for ((key, p) <- people) avg += p.totalDesc
@@ -182,6 +224,8 @@ object Network {
     val s = Source.fromFile(filePath)
     val people = (for (line <- s.getLines) yield line.split("\t")).toList.drop(1)
     val n = new Network(people)
+    n.updateChildren()
+    n.updateSiblings()
     n.updateDescendents()
     n
   }
@@ -192,12 +236,14 @@ object Network {
     println("avg descendents: " + n.avgDescendents)
     println("edge count: " + n.edges.size)
 
+    println("sibling counts: " + n.sibCounts)
+    /*
     n.shuffle(8)
 
     println(n)
     println("avg descendents: " + n.avgDescendents)
     println("edge count: " + n.edges.size)
 
-    n.save("test.txt")
+    n.save("test.txt") */
   }
 }
--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -12,15 +12,24 @@ class Person(data: Array[String]) {
 	var fatherId = data(3).toInt 
 	var motherId = data(4).toInt
 
+	var children = Set[Int]()
 	var descendents = Set[Int]()
+	var siblings = Set[Int]()
+
+	def addChild(childId: Int) = children += childId
+	def isChild(childId: Int): Boolean = children.contains(childId)
+	def clearChildren() = children = Set[Int]()
+	def totalChildren = children.size
 	
 	def addDescendent(descId: Int) = descendents += descId
-	
 	def isDescendent(descId: Int): Boolean = descendents.contains(descId)
-
 	def clearDescendents() = descendents = Set[Int]()
-
 	def totalDesc = descendents.size
+
+	def addSibling(sibId: Int) = siblings += sibId
+	def isSibling(sibId: Int): Boolean = siblings.contains(sibId)
+	def clearSiblings() = siblings = Set[Int]()
+	def totalSiblings = siblings.size
 	
 	def checkLoops: Boolean = (!(descendents contains fatherId)) && (!(descendents contains motherId))
 
